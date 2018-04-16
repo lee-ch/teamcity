c7da1c1fe71858e33eab79d5a75601ee97fd3d50
My Name <someemail@doesnotexist.com>
2018-04-15 22:39:42 -0600

    Updated Unzip-File script

--- Unzip-File.ps1
+++ Unzip-File.ps1
@@ -1,19 +1,18 @@
-param(
-    [string] $zipfile,
-    [string] $outdir
-)
+function Unzip($zipfile, $outdir) {
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    $archive = [System.IO.Compression.ZipFile]::OpenRead($zipfile)
+    foreach ($entry in $archive.Entries) {
+        $entryTargetFilePath = [System.IO.Path]::Combine($outdir, $entry.FullName)
+        $entryDir = [System.IO.Path]::GetDirectoryName($entryTargetFilePath)
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$archive = [System.IO.Compression.ZipFile]::OpenRead($zipfile)
-foreach ($entry in $archive.Entries) {
-    $entryTargetFilePath = [System.IO.Path]::Combine($outdir, $entry.FullName)
-    $entryDir = [System.IO.Path]::GetDirectoryName($entryTargetFilePath)
+        if (!(Test-Path $entryDir)) {
+            New-Item -ItemType Directory -Path $entryDir | Out-Null
+        }
 
-    if (!(Test-Path $entryDir)) {
-        New-Item -ItemType Directory -Path $entryDir | Out-Null
-    }
-
-    if (!$entryTargetFilePath.EndsWith("\")) {
-        [System.IO.Compression.ZipFileExtensions]::ExtractToFile($entry, $entryTargetFilePath, $true)
+        if (!$entryTargetFilePath.EndsWith("\")) {
+            [System.IO.Compression.ZipFileExtensions]::ExtractToFile($entry, $entryTargetFilePath, $true)
+        }
     }
 }
+
+Unzip -zipfile "%zipfile%" -outdir "%outdir%"
